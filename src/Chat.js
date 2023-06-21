05b0408717e0c045687588ecfcd8485a00eab57d
Pixel <petngalan@gmail.com>
2023-06-21 18:24:39 +0200

    Make the popups work and style them

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,10 @@
 import Message from "./Message";
 import "emoji-picker-element";
+import { useEffect } from "react";
+import uniqid from "uniqid";
+import insertText from "https://cdn.jsdelivr.net/npm/insert-text-at-cursor@0.3.0/index.js";
 
-function Chat({ title, lastMsg }) {
+function Chat({ openPopup, title, lastMsg }) {
   const messages = [
     {
       sender: "Web dev #1",
@@ -13,6 +16,13 @@ function Chat({ title, lastMsg }) {
     },
   ];
 
+  useEffect(() => {
+    const emojiPicker = document.querySelector("emoji-picker");
+    emojiPicker.addEventListener("emoji-click", (e) => {
+      insertText(document.querySelector("#msg-input"), e.detail.unicode);
+    });
+  }, []);
+
   return (
     <div className="chat">
       <div className="chat-header">
@@ -22,15 +32,15 @@ function Chat({ title, lastMsg }) {
           <p className="last-msg">Last message at {lastMsg.time}</p>
         </div>
         <div className="group-editor-wrapper">
-          <button>
-            <i class="fa-solid fa-ellipsis fa-xl"></i>
+          <button onClick={openPopup} className="popup-btn">
+            <i className="fa-solid fa-ellipsis fa-xl"></i>
           </button>
-          <div className="group-editor-popup">
+          <div className="group-editor-popup popup">
             <button>
-              <i class="fa-solid fa-plus fa-xl"></i> Add person
+              <i className="fa-solid fa-plus fa-xl"></i> Add person
             </button>
             <button>
-              <i class="fa-solid fa-right-from-bracket fa-xl"></i> Leave
+              <i className="fa-solid fa-right-from-bracket fa-xl"></i> Leave
             </button>
           </div>
         </div>
@@ -38,19 +48,23 @@ function Chat({ title, lastMsg }) {
 
       <div className="chat-content">
         {messages.map((msg) => (
-          <Message sender={msg.sender} content={msg.content} />
+          <Message key={uniqid()} sender={msg.sender} content={msg.content} />
         ))}
       </div>
 
       <div className="msg-form-wrapper">
         <form id="message-form">
-          <button type="button">
-            <i class="fa-solid fa-face-laugh-beam fa-xl"></i>
+          <button
+            onClick={openPopup}
+            type="button"
+            className="emoji-btn popup-btn"
+          >
+            <i className="fa-solid fa-face-laugh-beam fa-xl"></i>
           </button>
-          <emoji-picker></emoji-picker>
-          <input type="text" placeholder="Aa"></input>
+          <emoji-picker class="light popup"></emoji-picker>
+          <input id="msg-input" type="text" placeholder="Aa"></input>
           <button type="submit">
-            <i class="fa-solid fa-paper-plane fa-xl"></i>
+            <i className="fa-solid fa-paper-plane fa-xl"></i>
           </button>
         </form>
       </div>
--- src/UI.js
+++ src/UI.js
@@ -2,47 +2,75 @@ import Chat from "./Chat";
 import ChatButton from "./ChatButton";
 
 function UI() {
+  function openPopup(e) {
+    const clickedButton = e.target.closest("button");
+    const popup = clickedButton.nextElementSibling;
+    popup.classList.toggle("active");
+  }
+
+  function closePopups(e) {
+    const popups = document.querySelectorAll(".popup");
+
+    Array.from(popups).forEach(function (popup) {
+      if (
+        popup.classList.contains("active") &&
+        !popup.contains(e.target) &&
+        !e.target.closest(".popup-btn")
+      ) {
+        popup.classList.toggle("active");
+      }
+    });
+  }
+
   return (
-    <div className="UI">
+    <div onClick={closePopups} className="UI">
       <div className="sidebar">
         <div className="sidebar-header">
           <div className="profile-pic"></div>
           <h1>Chats</h1>
           <div className="sidebar-header-btns">
             <div className="menu-wrapper">
-              <button className="sidebar-header-btn">
-                <i class="fa-solid fa-ellipsis fa-xl"></i>
+              <button
+                onClick={openPopup}
+                className="sidebar-header-btn popup-btn"
+              >
+                <i className="fa-solid fa-ellipsis fa-xl"></i>
               </button>
-              <div className="menu-popup">
-                <button>
-                  <i class="fa-solid fa-gear fa-xl"></i> Preferences
+              <div className="menu-popup popup">
+                <button disabled>
+                  <i className="fa-solid fa-gear fa-xl"></i> Preferences
+                </button>
+                <button disabled>
+                  <i className="fa-solid fa-circle-question fa-xl"></i> Help
+                </button>
+                <button disabled>
+                  <i className="fa-solid fa-circle-exclamation fa-xl"></i>{" "}
+                  Report
                 </button>
                 <button>
-                  <i class="fa-solid fa-circle-question fa-xl"></i> Help
-                </button>
-                <button>
-                  <i class="fa-solid fa-circle-exclamation fa-xl"></i> Report
-                </button>
-                <button>
-                  <i class="fa-solid fa-right-from-bracket fa-xl"></i> Logout
+                  <i className="fa-solid fa-right-from-bracket fa-xl"></i>{" "}
+                  Logout
                 </button>
               </div>
             </div>
             <div className="add-chat-wrapper">
-              <button className="sidebar-header-btn">
-                <i class="fa-solid fa-plus fa-xl"></i>
+              <button
+                onClick={openPopup}
+                className="sidebar-header-btn popup-btn"
+              >
+                <i className="fa-solid fa-plus fa-xl"></i>
               </button>
-              <div className="add-chat-popup">
+              <div className="add-chat-popup popup">
                 <button>
-                  <i class="fa-solid fa-user fa-xl"></i> Private
+                  <i className="fa-solid fa-user fa-xl"></i> Private
                 </button>
                 <button>
-                  <i class="fa-solid fa-users fa-xl"></i> Public
+                  <i className="fa-solid fa-users fa-xl"></i> Public
                 </button>
               </div>
             </div>
             <button className="sidebar-header-btn">
-              <i class="fa-solid fa-moon fa-xl"></i>
+              <i className="fa-solid fa-moon fa-xl"></i>
             </button>
           </div>
         </div>
@@ -52,7 +80,9 @@ function UI() {
           lastMsg={{ content: "hi", time: "20/06 18:18" }}
         />
       </div>
+
       <Chat
+        openPopup={openPopup}
         title={"Test Chat"}
         lastMsg={{ content: "hi", time: "20/06 18:18" }}
       />
